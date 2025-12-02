43cbb85fb498f1ca62bf56b43858faefd4882b89
minhlh <minhhl298.st@gmail.com>
2025-12-02 12:13:43 +0000

    feat: base

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+// comment: NestJS imports
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+
+  // comment: global prefix
+  app.setGlobalPrefix("api");
+
+
+  // comment: Swagger config
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(process.env.APP_NAME || "NestJS API")
+    .setDescription("Stayly Bookings API Documentation")
+    .setVersion("1.0")
+    .addBearerAuth(
+
+      {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        name: "JWT",
+
+        description: "Enter JWT token",
+        in: "header",
+      },
+
+      "JWT-auth"
+
+    )
+    .addTag("health")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup("api/docs", app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
+
